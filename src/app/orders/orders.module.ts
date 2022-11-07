4e1cd9eaf0dbf8fc683bfbc6435b71701407d5dc
dnyuhm <dylanmeuriot@gmail.com>
2022-11-07 14:13:05 +0100

    Commit 3 : pages

--- src/app/orders/orders.module.ts
+++ src/app/orders/orders.module.ts
@@ -2,9 +2,18 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 
 import { OrdersRoutingModule } from './orders-routing.module';
+import { PageListOrdersComponent } from './pages/page-list-orders/page-list-orders.component';
+import { PageAddOrderComponent } from './pages/page-add-order/page-add-order.component';
+import { PageEditOrderComponent } from './pages/page-edit-order/page-edit-order.component';
+import { PageTestComponent } from './pages/page-test/page-test.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    PageListOrdersComponent,
+    PageAddOrderComponent,
+    PageEditOrderComponent,
+    PageTestComponent
+  ],
   imports: [CommonModule, OrdersRoutingModule],
 })
 export class OrdersModule {}
