6837fce5dc63837fa9fb79ff7c82be61d3aaca54
dnyuhm <dylanmeuriot@gmail.com>
2022-11-07 16:44:50 +0100

    Commit 9 : Router angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageListOrdersComponent } from './orders/pages/page-list-orders/page-list-orders.component';
+import { PageTestComponent } from './orders/pages/page-test/page-test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'orders', component: PageListOrdersComponent },
+  { path: 'test', component: PageTestComponent },
+  { path: '', redirectTo: 'orders', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
