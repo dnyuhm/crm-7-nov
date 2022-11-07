f94b6bcce2e3a9efbc4f020f75b6ede8c741b135
dnyuhm <dylanmeuriot@gmail.com>
2022-11-07 14:47:02 +0100

    Commit 6 : Header, Nav & Footer

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { OrdersModule } from '../orders/orders.module';
+import { HeaderComponent } from './components/header/header.component';
+import { NavComponent } from './components/nav/nav.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HeaderComponent,
+    NavComponent,
+    FooterComponent
+  ],
   imports: [CommonModule],
-  exports: [OrdersModule],
+  exports: [OrdersModule, HeaderComponent, NavComponent, FooterComponent],
 })
 export class CoreModule {}
