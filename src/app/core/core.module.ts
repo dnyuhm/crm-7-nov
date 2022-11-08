df8fb49e0466f1ad24cca425cee3988d264a8e6e
dnyuhm <dylanmeuriot@gmail.com>
2022-11-08 09:54:03 +0100

    Commit 13 : Navigation

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,17 +1,14 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { OrdersModule } from '../orders/orders.module';
+import { FooterComponent } from './components/footer/footer.component';
 import { HeaderComponent } from './components/header/header.component';
 import { NavComponent } from './components/nav/nav.component';
-import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
-  declarations: [
-    HeaderComponent,
-    NavComponent,
-    FooterComponent
-  ],
-  imports: [CommonModule],
+  declarations: [HeaderComponent, NavComponent, FooterComponent],
+  imports: [CommonModule, RouterModule],
   exports: [OrdersModule, HeaderComponent, NavComponent, FooterComponent],
 })
 export class CoreModule {}
