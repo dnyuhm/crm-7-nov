654a24ddf861a5563425cebcfabb056b8d4d773c
dnyuhm <dylanmeuriot@gmail.com>
2022-11-08 12:15:52 +0100

    Commit 17 : Observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -8,10 +9,17 @@ import { Component } from '@angular/core';
 export class AppComponent {
   public title: string;
   public close: boolean;
+  private obs: Observable<number>;
 
   constructor() {
     this.title = 'crm';
     this.close = false;
+    this.obs = new Observable<number>((listX) => {
+      listX.next(Math.random());
+    });
+
+    // this.obs.subscribe((data) => console.log(data));
+    // this.obs.subscribe((data) => console.log(data));
   }
 
   public toggle(): void {
